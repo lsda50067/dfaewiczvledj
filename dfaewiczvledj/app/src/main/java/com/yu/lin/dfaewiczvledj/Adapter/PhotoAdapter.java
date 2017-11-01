8017a43181b1325d1805690e09a3e9396fd57060
Yu <lsda10108017@gmail.com>
2017-11-01 12:59:53 +0800

    null adapter

---
+++ dfaewiczvledj/app/src/main/java/com/yu/lin/dfaewiczvledj/Adapter/PhotoAdapter.java
@@ -0,0 +1,34 @@
+package com.yu.lin.dfaewiczvledj.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Lin-Yu on 01/11/2017.
+ *
+ */
+
+public class PhotoAdapter extends BaseAdapter {
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
