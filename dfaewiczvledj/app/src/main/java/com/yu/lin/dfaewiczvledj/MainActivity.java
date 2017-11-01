4ce5c5e8639ff1222428187b4845ee82e118a65d
Yu <lsda10108017@gmail.com>
2017-11-01 12:59:19 +0800

    for Test ImageList

--- dfaewiczvledj/app/src/main/java/com/yu/lin/dfaewiczvledj/MainActivity.java
+++ dfaewiczvledj/app/src/main/java/com/yu/lin/dfaewiczvledj/MainActivity.java
@@ -2,12 +2,30 @@ package com.yu.lin.dfaewiczvledj;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.yu.lin.dfaewiczvledj.provider.ImageList;
+
+import java.util.List;
+
+/**
+ * Created by Lin-Yu on 31/10/2017.
+ *
+ */
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private static List<Integer> imageList = ImageList.getDrawableIdList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Log.d(TAG, "onCreate: " + imageList);
     }
+
+
+
 }
