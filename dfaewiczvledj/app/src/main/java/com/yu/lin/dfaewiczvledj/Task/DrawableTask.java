46b19db6aff15babc7f51435090a1b900dc76c9a
Yu <lsda10108017@gmail.com>
2017-11-01 12:59:39 +0800

    add Task

---
+++ dfaewiczvledj/app/src/main/java/com/yu/lin/dfaewiczvledj/Task/DrawableTask.java
@@ -0,0 +1,48 @@
+package com.yu.lin.dfaewiczvledj.Task;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Created by Lin-Yu on 01/11/2017.
+ *
+ */
+
+public class DrawableTask extends AsyncTask<Integer, Void, Drawable> {
+
+    @SuppressLint("StaticFieldLeak")
+    private Context mContext;
+    private final WeakReference<ImageView> imageViewReference;
+
+    public DrawableTask(ImageView imageView, Context context) {
+        this.mContext = context;
+        this.imageViewReference = new WeakReference<ImageView>(imageView);
+    }
+
+    @Override
+    protected Drawable doInBackground(Integer... integer) {
+        // integer[0] is drawableId
+        return mContext.getResources().getDrawable(integer[0]);
+    }
+
+    @Override
+    // Once the image is downloaded, associates it to the imageView
+    protected void onPostExecute(Drawable drawable) {
+        if (isCancelled()) {
+            drawable = null;
+        }
+
+        if (imageViewReference != null) {
+            ImageView imageView = imageViewReference.get();
+            if (imageView != null) {
+                imageView.setImageDrawable(drawable);
+            }
+        }
+    }
+}
