04fbfcd978c85458dea1f64b0b8f96861bd176f0
Yu <lsda10108017@gmail.com>
2017-11-01 13:00:54 +0800

    new Image List

---
+++ dfaewiczvledj/app/src/main/java/com/yu/lin/dfaewiczvledj/provider/ImageList.java
@@ -0,0 +1,79 @@
+package com.yu.lin.dfaewiczvledj.provider;
+
+import com.yu.lin.dfaewiczvledj.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lin-Yu on 01/11/2017.
+ *
+ */
+
+public class ImageList {
+
+    private static List<Integer> images = new ArrayList<>();
+    private static List<Integer> imageId = new ArrayList<>();
+
+    public static List<Integer> getDrawableIdList() {
+        return getImages();
+    }
+
+    public static List<Integer> getImageId(){
+        // remember remove
+        return null;
+    }
+
+    private static List<Integer> getImages(){
+        images.add(R.drawable.hd_wallpaper_preview_1);
+        images.add(R.drawable.hd_wallpaper_preview_2);
+        images.add(R.drawable.hd_wallpaper_preview_3);
+        images.add(R.drawable.hd_wallpaper_preview_4);
+        images.add(R.drawable.hd_wallpaper_preview_5);
+        images.add(R.drawable.hd_wallpaper_preview_6);
+        images.add(R.drawable.hd_wallpaper_preview_7);
+        images.add(R.drawable.hd_wallpaper_preview_8);
+        images.add(R.drawable.hd_wallpaper_preview_9);
+        images.add(R.drawable.hd_wallpaper_preview_10);
+        images.add(R.drawable.hd_wallpaper_preview_11);
+        images.add(R.drawable.hd_wallpaper_preview_12);
+        images.add(R.drawable.hd_wallpaper_preview_13);
+        images.add(R.drawable.hd_wallpaper_preview_14);
+        images.add(R.drawable.hd_wallpaper_preview_15);
+        images.add(R.drawable.hd_wallpaper_preview_16);
+        images.add(R.drawable.hd_wallpaper_preview_17);
+        images.add(R.drawable.hd_wallpaper_preview_18);
+        images.add(R.drawable.hd_wallpaper_preview_19);
+        images.add(R.drawable.hd_wallpaper_preview_20);
+        images.add(R.drawable.hd_wallpaper_preview_21);
+        images.add(R.drawable.hd_wallpaper_preview_22);
+        images.add(R.drawable.hd_wallpaper_preview_23);
+        images.add(R.drawable.hd_wallpaper_preview_24);
+        images.add(R.drawable.hd_wallpaper_preview_25);
+        images.add(R.drawable.hd_wallpaper_preview_26);
+        images.add(R.drawable.hd_wallpaper_preview_27);
+        images.add(R.drawable.hd_wallpaper_preview_28);
+        images.add(R.drawable.hd_wallpaper_preview_29);
+        images.add(R.drawable.hd_wallpaper_preview_30);
+        images.add(R.drawable.hd_wallpaper_preview_31);
+        images.add(R.drawable.hd_wallpaper_preview_32);
+        images.add(R.drawable.hd_wallpaper_preview_33);
+        images.add(R.drawable.hd_wallpaper_preview_34);
+        images.add(R.drawable.hd_wallpaper_preview_35);
+        images.add(R.drawable.hd_wallpaper_preview_36);
+        images.add(R.drawable.hd_wallpaper_preview_37);
+        images.add(R.drawable.hd_wallpaper_preview_38);
+        images.add(R.drawable.hd_wallpaper_preview_39);
+        images.add(R.drawable.hd_wallpaper_preview_40);
+        images.add(R.drawable.hd_wallpaper_preview_41);
+        images.add(R.drawable.hd_wallpaper_preview_42);
+        images.add(R.drawable.hd_wallpaper_preview_43);
+        images.add(R.drawable.hd_wallpaper_preview_44);
+        images.add(R.drawable.hd_wallpaper_preview_45);
+        images.add(R.drawable.hd_wallpaper_preview_47);
+        images.add(R.drawable.hd_wallpaper_preview_48);
+        images.add(R.drawable.hd_wallpaper_preview_49);
+        images.add(R.drawable.hd_wallpaper_preview_50);
+        return images;
+    }
+}
